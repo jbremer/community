bcaa940b048718873e2f7bc0560c4a49d56612a2
Jurriaan Bremer <jbr@cuckoo.sh>
2017-02-16 16:19:23 +0100

    test .js dropping .ps1 against raised exceptions

--- modules/signatures/windows/unittest.py
+++ modules/signatures/windows/unittest.py
@@ -143,6 +143,26 @@ class BreakpipeUnittest(NamedUnittest):
             count += funcs.get("MessageBoxTimeoutA", 0)
         return count == 2
 
+class Javascript(NamedUnittest):
+    description = "Various Javascripts scripts & tricks"
+
+class PowershellDropperJavascript(Javascript):
+    name = "unittest.named.javascript.fpo"
+    description = (
+        "Tests that PowerShell doesn't crash due to an unhandled exception "
+        "(namely 0xe0434f4d, a COM exception thrown around in PowerShell)"
+    )
+
+    def init(self):
+        self.raises_exception = False
+
+    def on_signature(self, sig):
+        if sig.name == "raises_exception":
+            self.raises_exception = True
+
+    def check(self):
+        return not self.raises_exception
+
 class ConnectLocal(object):
     filter_apinames = "connect",
     should_connect = None
